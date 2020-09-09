5911c8bd467b66da8bbd02e902c38b1fe9e751dc
yimina <kunachiyo@gmail.com>
2020-09-09 14:51:25 +0900

    add file

---
+++ src/main/java/com/semantic/annotator/resourceDTO/AirQualityObservedDTO.java
@@ -0,0 +1,32 @@
+package com.semantic.annotator.resourceDTO;
+
+public class AirQualityObservedDTO {
+
+    private String id;              //getId()   
+    private String observedValue1;  //getAirQualityObservation().getValue()
+    private String observedValue2;  //getAirQualityIndexObservation().getValue()
+    
+    private String indexRef;        //getIndexRef().getValue();
+
+    private String evaluationValue1; //getAirQualityObservation().getValue()
+    private String evaluationValue2; //getAirQualityIndexObservation().getValue()
+    
+    private String locationName1 = "addressCountry";
+    private String addressCountry;      //getAddress().getValue().getAddressCountry()
+    private String locationName2 = "addressRegion";
+    private String addressRegion;       //getAddress().getValue().getAddressRegion()
+    private String locationName3 = "addressLocality";
+    private String addressLocality;     //getAddress().getValue().getAddressLocality()
+    private String locationName4 = "streetAddress";
+    private String streetAddress;       //getAddress().getValue().getStreetAddress()
+    private String locationName5 = "addressTown";
+    private String addressTown;         //getAddress().getValue().getAddressTown()
+
+    private String locationType;      //getLocation().getValue().getType()
+    private String latitute;     
+    private String longitude;    
+
+    private String createdAt;       //offStreetParkingList.get(0).getCreatedAt().split(",")[0]+"+09:00"
+    private String modifiedAt;      //getModifiedAt().split(",")[0]+"+09:00"
+
+}
