205ea3687f9beca00b7b2e8d61a851cc560ced76
yimina <kunachiyo@gmail.com>
2020-09-09 14:52:10 +0900

    modified

--- src/main/java/com/semantic/annotator/resource/AirQualityObserved.java
+++ src/main/java/com/semantic/annotator/resource/AirQualityObserved.java
@@ -15,7 +15,7 @@ public class AirQualityObserved {
     private class AirQualityObservation {
         private String observedAt;
         private String type;
-        private Object value;
+        private AirQualityObservationValue value;
         public class AirQualityObservationValue {
             private Number no2;
             private Number o3;
@@ -23,7 +23,43 @@ public class AirQualityObserved {
             private Number so2;
             private Number pm10;
             private Number co;
+
+            public Number getNO2() {
+                return no2;
+            }
+
+            public Number getO3() {
+                return o3;
+            }
+
+            public Number getPM25() {
+                return pm25;
+            }
+
+            public Number getSO2() {
+                return so2;
+            }
+
+            public Number getPM10() {
+                return pm10;
+            }
+
+            public Number getCO() {
+                return co;
+            }
         }
+
+        public AirQualityObservationValue getValue() {
+            return value;
+        }
+
+        public String getObservedAt () {
+            return observedAt;
+        }
+    }
+
+    public AirQualityObservation getAirQualityObservation() {
+        return airQualityObservation;
     }
 
     private class Location {
@@ -32,7 +68,23 @@ public class AirQualityObserved {
         public class LocationValue {
             private String type;
             private Object coordinates;
+        
+            public String getType() {
+                return type;
+            }
+
+            public Object getCoordinates() {
+                return coordinates;
+            }
+       }
+
+        public LocationValue getValue() {
+            return value;
         }
+    }
+
+    public Location getLocation() {
+        return location;
     }
 
     public class AirQualityIndexObservation {
@@ -48,12 +100,64 @@ public class AirQualityObserved {
             private String no2Category;
             private String coCategory;
             private String so2Category;
+
+            public String getPM10Category() {
+                return pm10Category;
+            }
+
+            public String getTotalCategory() {
+                return totalCategory;
+            }
+
+            public Number getTotalIndex() {
+                return totalIndex;
+            }
+
+            public String getO3Category() {
+                return o3Category;
+            }
+
+            public String getPM25Category() {
+                return pm25Category;
+            }
+
+            public String getNO2Category() {
+                return no2Category;
+            }
+
+            public String getCOCategory() {
+                return coCategory;
+            }
+
+            public String getSO2Category() {
+                return so2Category;
+            }
         }
+
+        public AirQualityIndexObservationValue getValue() {
+            return value;
+        }
+
+        public String getObservedAt() {
+            return observedAt;
+        }
+    }
+
+    public AirQualityIndexObservation getAirQualityIndexObservation() {
+        return airQualityIndexObservation
     }
 
     public class IndexRef {
         private String type;
         private String value;
+
+        public Object getValue() {
+            return value;
+        }
+    }
+
+    public IndexRef getIndexRef() {
+        return indexRef;
     }
 
     public class Address {
@@ -65,14 +169,70 @@ public class AirQualityObserved {
             private String addressLocality;
             private String addressRegion;
             private String addressTown;
+
+            public String getAddressCountry() {
+                return addressCountry;
+            }
+
+            public String getAddressRegion() {
+                return addressRegion;
+            }
+
+            public String getAddressLocality() {
+                return addressLocality;
+            }
+
+            public String getStreetAddress() {
+                return streetAddress;
+            }
+
+            public String getAddressTown() {
+                return addressTown;
+            }
         }
+
+        public AddressValue getValue() {
+            return value;
+        }
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public Object getAddressCountry() {
+        return address.value.addressCountry;
+    }
+
+    public Object getAddressRegion() {
+        return address.value.addressRegion;
+    }
+
+    public Object getAddressLocality() {
+        return address.value.addressLocality;
+    }
+
+    public Object getStreetAddress() {
+        return address.value.streetAddress;
+    }
+
+    public Object getAddressTown() {
+        return address.value.addressTown;
     }
 
     public String getId() {
         return id;
     }
 
-    public Object getLocation() {
-        return location;
+    public String getType() {
+        return type;
+    }
+
+    public String getModifiedAt() {
+        return modifiedAt;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
     }
 }
