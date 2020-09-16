ba07563a66b61bef6482ef469aec0024d7de1719
jieun <love9ly@gmail.com>
2020-09-16 17:25:04 +0900

    weatherObserved annotation completed

--- src/main/java/com/semantic/annotator/resourceDTO/WeatherObservedDTO.java
+++ src/main/java/com/semantic/annotator/resourceDTO/WeatherObservedDTO.java
@@ -99,4 +99,30 @@ public class WeatherObservedDTO {
     public void setObservedAt(String observedAt) {
         this.observedAt = observedAt.split(",")[0]+"+09:00";
     }
+
+    public String getOrder(int i) {
+        String result = "";
+        switch (i) {
+            case 0: result = id; break;
+            case 1: result = weatherObservation; break;
+            case 2: result = weatherEvaluationHasRecord; break;
+            case 3: result = addressName1; break;
+            case 4: result = addressCountry; break;
+            case 5: result = addressName2; break;
+            case 6: result = addressRegion; break;
+            case 7: result = addressName3; break;
+            case 8: result = addressLocality; break;
+            case 9: result = addressName4; break;
+            case 10: result = streetAddress; break;
+            case 11: result = addressName5; break;
+            case 12: result = addressTown; break;
+            case 13: result = locationType; break;
+            case 14: result = latitute; break;
+            case 15: result = longitude; break;
+            case 16: result = modifiedAt; break;
+            case 17: result = createdAt; break;
+            case 18: result = observedAt; break;
+        }
+        return result;
+    }
 }
